d91095aa8b517cbe0a761e39e3c5e11d064ffd12
Dmitry Ryzhkov <mation.me@icloud.com>
2023-06-09 14:40:39 +0200

    refactor

--- src/models/Zone.js
+++ src/models/Zone.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const ZoneSchema = new mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true, default: '' },
-  devices: { type: Array, default: [] },
-  isActive: { type: Boolean, default: false },
-  isEnabled: { type: Boolean, default: true },
-  floor: { type: String, default: '' },
-  space: { type: String, default: '' },
-  volumeBar: {
+  devices: { type: Array, default: [] }, // устройства в зоне
+  isActive: { type: Boolean, default: false }, // флаг, указывающий, активна ли зона
+  isEnabled: { type: Boolean, default: true }, // флаг, указывающий, доступна ли зона
+  floor: { type: String, default: '' }, // этаж, на котором находится зона
+  space: { type: String, default: '' }, // пространство, к которому относится зона
+  volumeBar: { // параметры панели управления громкостью в зоне
     type: Object, default: {
       "visible": false,
       "template": 0,
@@ -16,7 +16,7 @@ const ZoneSchema = new mongoose.Schema({
       "mute": false
     }
   },
-  settings: { type: Object, default: {} }
+  settings: { type: Object, default: {} } // настройки зоны
 });
 
 module.exports = mongoose.model('Zone', ZoneSchema);
--- src/models/source.js
+++ src/models/source.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const SourceSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, default: '' },
-  driver: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver', required: true },
+  driver: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver', required: true }, // драйвер, связанный с источником
   image: { type: String, default: '' },
-  isActive: { type: Boolean, default: false },
-  isEnabled: { type: Boolean, default: true },
-  isVisible: { type: Boolean, default: true },
-  zones: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Zone' }], default: [] },
+  isActive: { type: Boolean, default: false }, // флаг, указывающий, активен ли источник
+  isEnabled: { type: Boolean, default: true }, // флаг, указывающий, доступен ли источник
+  controlEnabled: { type: Boolean, default: true }, // флаг, указывающий, доступно ли управление зоной через панель
+  isVisible: { type: Boolean, default: true }, // флаг, указывающий, видим ли источник на панели
+  zones: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Zone' }], default: [] }, // зоны, связанные с источником
 });
 
 module.exports = mongoose.model('Source', SourceSchema);
